1bf86dcfb3f3f16a979e876143e26061cc14bdaf
Tosh Miller <miller.tosh@yahoo.com>
2019-09-08 14:58:51 -0700

    Add project files.

---
+++ LayeredGIF/LayeredGIF.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Tosh Miller (prophecycat)
+*/
+
+#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+
+using namespace std;
+
+const wchar_t CLASS_NAME[] = L"Window Class";
+WNDCLASS wc;
+
+void createWindow()
+{
+}
+
+int main()
+{
+}
