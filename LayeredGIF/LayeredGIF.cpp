f170c6f8ed3eaf979ba34b74c22edf8fc0be0b63
Tosh Layne Miller <tosh.miller@wsu.edu>
2019-09-10 19:06:07 -0700

    Added looping even which checks for window exit button being clicked.

--- LayeredGIF/LayeredGIF.cpp
+++ LayeredGIF/LayeredGIF.cpp
@@ -3,10 +3,28 @@ Author: Tosh Miller (prophecycat)
 */
 
 #include <iostream>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
-using namespace std;
 
+/*
+ * Runs loop which checks for SDL_QUIT event (exit button clicked) and then returns
+ */
+void check_quit(){
+	SDL_Event e;
+	bool quit = false;
+	while (!quit){
+	    while (SDL_PollEvent(&e)){
+	        if (e.type == SDL_QUIT){
+	            quit = true;
+	        }
+	    }
+	}
+}
+
+
+/*
+ * Create general window using
+ */
 void createWindow()
 {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -14,12 +32,17 @@ void createWindow()
 	SDL_Window* window = SDL_CreateWindow
 	("Layered GIF", // window's title
 		10, 25, // coordinates on the screen, in pixels, of the window's upper left corner
-		640, 480, // window's length and height in pixels  
+		640, 480, // window's length and height in pixels
 		SDL_WINDOW_OPENGL);
+
+	// loops until exit button is clicked and destroys window
+	check_quit();
 
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
+
+
 
 int main(int argc, char* argv[])
 {
